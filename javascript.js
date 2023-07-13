50fb7ebbafc9186d42162b1400113fab8d9adfc3
최선희 <choiyoface@gmail.com>
2023-07-13 20:58:18 +0200

    working with the buttons

--- javascript.js
+++ javascript.js
@@ -1,27 +1,53 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-   
-    createBoard(32);
 
-    function createBoard(size){
+let color="black";
+getSize();
+
+function createBoard(size){
     const board = document.querySelector(".board");
+    
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-    ;
-
-    let numDivs = size * size;
-    for ( let i = 0; i<numDivs; i++){
-        let div = document.createElement('div');
-        div.style.backgroundColor = "white";
-        board.insertAdjacentElement("beforeend", div);
-    };
-
-    }
     
 
 
 
+    let numDivs = size * size;
 
+    for ( let i = 0; i<numDivs; i++){
+        let div = document.createElement('div');
+        div.style.backgroundColor = "white";
+        div.addEventListener("mouseover", colorDiv)
+        board.insertAdjacentElement("beforeend", div);
+    }
+ }
 
+createBoard(16);
 
+function getSize(input){
+        
+    let message = document.getElementById("message")
+    if(input >=2 && input <=100){
+        createBoard(input);
+    }
+    else if (input == "" || input < 2 ||input > 100 ){
+        message.innerHTML = "Provide a number between 2 and 100"
+    };
+    }
 
-})
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }else{
+        this.style.backgroundColor = "black";
+    }
+
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll('.board div');
+    divs.forEach((div)=>div.style.backgroundColor = "white")
+}
